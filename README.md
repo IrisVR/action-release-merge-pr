9e57b4ed12cf2dcd726d3f1b22a7310b28533212
Graham Hagger <graham@irisvr.com>
2020-08-31 09:14:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# action-release-merge-prd
+
+GitHub action to automatically create or update a PR from the source branch into a target branch every time a change is made. A webhook can be configured to send both a successful or failure notification to Slack.
+
+## Inputs
+
+### `source`
+
+**Required** Source branch for the merge
+
+### `target`
+
+**Required** Target branch for the merge
+
+### `webhook_url`
+
+**Optional** The Slack webhook URL to post to.
+
+## Example usage
+
+```
+name: Update cms/master
+
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  update-cms-master:
+    name: Merge master into release after a PR is merged
+    runs-on: ubuntu-latest
+    steps:
+      - name: checkout
+        uses: actions/checkout@v2
+      - name: merge
+        uses: irisvr/action-release-merge-pr@v1
+        id: merge
+        with:
+          github_token: ${{ github.token }}
+          source: 'master'
+          target: 'release'
+          webhook_url: #if present add the Slack webhook
+```
+
+## Build
+
+```bash
+npm run-script build
+```
---
+++ src/index.js
@@ -0,0 +1,149 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+const { Octokit } = require("@octokit/rest");
+const slack = require("slack-notify")(core.getInput("webhook_url"));
+
+const token = core.getInput("github_token");
+const octokit = new Octokit({ auth: token });
+const repo = github.context.repo;
+
+function slackSuccessMessage(source, target, prUrl, status) {
+  return {
+    color: "#27ae60",
+    icon: ":git:",
+    message: `PR to merge ${source} into ${target} created/updated.`,
+    description: `${prUrl}`,
+  };
+}
+
+function slackErrorMessage(source, target, status) {
+  return {
+    color: "#C0392A",
+    icon: ":alert:",
+    message: `Error creating PR merging ${source} into ${target}`,
+    description: ":face_with_head_bandage: Fix me please :pray:",
+  };
+}
+
+async function slackMessage(source, target, prUrl, status) {
+  if (core.getInput("webhook_url")) {
+    const slack = require("slack-notify")(core.getInput("webhook_url"));
+
+    let payload =
+      status == "success"
+        ? slackSuccessMessage(source, target, status)
+        : slackErrorMessage(source, target, status);
+
+    slack.send({
+      icon_emoji: payload.icon,
+      username: payload.message,
+      attachments: [
+        {
+          author_name: github.context.payload.repository.full_name,
+          author_link: `https://github.com/${github.context.payload.repository.full_name}/`,
+          title: payload.message,
+          text: payload.description,
+          color: payload.color,
+          fields: [{ title: "Job Status", value: status, short: false }],
+        },
+      ],
+    });
+  }
+}
+
+async function createBranch(octokit, context, branch) {
+  try {
+    await octokit.repos.getBranch({
+      ...context.repo,
+      branch,
+    });
+  } catch (error) {
+    if (error.name === "HttpError" && error.status === 404) {
+      await octokit.git.createRef({
+        ref: `refs/heads/${branch}`,
+        sha: context.sha,
+        ...context.repo,
+      });
+    } else {
+      console.log("Error while creating new branch");
+      throw Error(error);
+    }
+  }
+}
+
+async function run() {
+  try {
+    const sourceBranch = core.getInput("SOURCE_BRANCH", { required: true });
+    const targetBranches = core.getInput("TARGET_BRANCH", { required: true });
+    const githubToken = core.getInput("GITHUB_TOKEN", { required: true });
+
+    const targetBranchesArray = targetBranches.split(",");
+
+    for (let branch of targetBranchesArray) {
+      console.log(`Making a pull request for ${branch} from ${sourceBranch}.`);
+      const {
+        payload: { repository },
+      } = github.context;
+
+      const octokit = new github.GitHub(githubToken);
+      //part of test
+      const { data: currentPulls } = await octokit.pulls.list({
+        owner: repository.owner.login,
+        repo: repository.name,
+      });
+      //create new branch from master branch and PR between new branch and target branch
+
+      const context = github.context;
+      const newBranch = `${branch}-sync-${context.sha.slice(-4)}`;
+      await createBranch(octokit, context, newBranch);
+
+      const currentPull = currentPulls.find((pull) => {
+        return pull.head.ref === newBranch && pull.base.ref === branch;
+      });
+
+      if (!currentPull) {
+        const { data: pullRequest } = await octokit.pulls.create({
+          owner: repository.owner.login,
+          repo: repository.name,
+          head: newBranch,
+          base: branch,
+          title: `sync: ${branch}  with ${newBranch}`,
+          body: `sync-branches: syncing branch with ${newBranch}`,
+          draft: false,
+        });
+
+        console.log(
+          `Pull request (${pullRequest.number}) successful! You can view it here: ${pullRequest.url}.`
+        );
+
+        core.setOutput("PULL_REQUEST_URL", pullRequest.url.toString());
+        core.setOutput("PULL_REQUEST_NUMBER", pullRequest.number.toString());
+        await slackMessage(
+          source,
+          target,
+          pullRequest.url.toString(),
+          "success"
+        );
+      } else {
+        console.log(
+          `There is already a pull request (${currentPull.number}) to ${branch} from ${newBranch}.`,
+          `You can view it here: ${currentPull.url}`
+        );
+
+        core.setOutput("PULL_REQUEST_URL", currentPull.url.toString());
+        core.setOutput("PULL_REQUEST_NUMBER", currentPull.number.toString());
+        await slackMessage(
+          source,
+          target,
+          currentPull.url.toString(),
+          "success"
+        );
+      }
+    }
+  } catch (error) {
+    await slackMessage(source, target, "", "failure");
+    core.setFailed(error.message);
+  }
+}
+
+run();
